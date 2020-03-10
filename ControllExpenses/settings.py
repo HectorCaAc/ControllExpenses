f6577556d81c1f6cf51875f034e0349a5b929dfd
Hector <hectoracosta0106@gmail.com>
2020-03-09 23:11:50 -0700

    Add django_heroku function at the end

--- ControllExpenses/settings.py
+++ ControllExpenses/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
@@ -133,3 +133,4 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticProduction')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+django_heroku.settings(locals())
