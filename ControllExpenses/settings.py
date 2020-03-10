7cd10b0d6aaa3921eb89e08a0d8fba4ea7a7a498
Hector <hectoracosta0106@gmail.com>
2020-03-09 22:33:38 -0700

    Remove celery

--- ControllExpenses/settings.py
+++ ControllExpenses/settings.py
@@ -104,12 +104,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 #Code add for the celery
 
-BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Africa/Nairobi'
+# BROKER_URL = 'redis://localhost:6379'
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+# CELERY_ACCEPT_CONTENT = ['application/json']
+# CELERY_TASK_SERIALIZER = 'json'
+# CELERY_RESULT_SERIALIZER = 'json'
+# CELERY_TIMEZONE = 'Africa/Nairobi'
 
 
 # Internationalization
