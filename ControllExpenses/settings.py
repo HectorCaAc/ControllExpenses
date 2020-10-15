e4d1e7d64914ea9bd7f137b2c011c8851f864222
HectorCaAc <hectoracosta0106@gmail.com>
2020-10-14 22:11:23 -0700

    Settings for react

--- ControllExpenses/settings.py
+++ ControllExpenses/settings.py
@@ -131,7 +131,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticProduction')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR,'frontend','dist')
 ]
 #django_heroku.settings(locals())
 
